500d03b4f222cb8597e13d9fd0b8c5a6d2f1c1ec
ShabanK <nooneyouwillknow@gmail.com>
2019-11-20 18:38:33 +0500

    okay pipes

--- pipes.js
+++ pipes.js
@@ -1,8 +1,16 @@
 const http = require("http");
 const fs = require("fs");
 
-const reader = fs.createReadStream(`${__dirname}/lorem.txt`, "utf8");
-const writer = fs.createWriteStream(`${__dirname}/pipedLorem.txt`);
+// const writer = fs.createWriteStream(`${__dirname}/pipedLorem.txt`);
 
-reader.pipe(writer);
-//pipe from readable to writeable
+// reader.pipe(writer);
+// //pipe from readable to writeable
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  const reader = fs.createReadStream(`${__dirname}/lorem.txt`, "utf8");
+  reader.pipe(res);
+});
+server.listen(3000, "127.0.0.1", () => {
+  console.log("Server is live");
+});
